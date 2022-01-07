19bdf7cb849a847fb1eb846e392ffbb62389af27
shivendushukre <shivendu2711@gmail.com>
2022-01-07 13:34:36 +0530

    updated py version

--- email_automation/settings.py
+++ email_automation/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://automaticemailapp.herokuapp.com',
+                 '127.0.0.1']
 
 
 # Application definition
